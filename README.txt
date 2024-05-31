4b91f3e791fd3864415043ffd2d362454a64b4fc
Abdul Rizal Subagja <71943144+rizalsih@users.noreply.github.com>
2024-05-31 20:29:38 +0700

    Add files via upload

---
+++ README.txt
@@ -0,0 +1,6 @@
+DESIGNED & DEVELOPED by GetTemplates.co
+
+Website: http://gettemplates.co/
+Pages: https://rizalsih.github.io/wedding-puspaahmad/
+
+Thankyou!
---
+++ js/music.js
@@ -0,0 +1,97 @@
+let now_playing = document.querySelector('.now-playing');
+let track_art = document.querySelector('.track-art');
+
+let playpause_btn = document.querySelector('.playpause-track');
+
+let seek_slider = document.querySelector('.seek_slider');
+let curr_time = document.querySelector('.current-time');
+let total_duration = document.querySelector('.total-duration');
+let curr_track = document.createElement('audio');
+
+let track_index = 0;
+let isPlaying = false;
+let isRandom = false;
+let updateTimer;
+
+const music_list = [
+  {
+    music: 'audio/audio.mp3'
+  }
+];
+
+loadTrack(track_index);
+
+function loadTrack(track_index) {
+  clearInterval(updateTimer);
+  reset();
+
+  curr_track.src = music_list[track_index].music;
+  curr_track.load();
+
+
+}
+
+function random_bg_color() {
+  let hex = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e'];
+  let a;
+
+  function populate(a) {
+    for (let i = 0; i < 6; i++) {
+      let x = Math.round(Math.random() * 14);
+      let y = hex[x];
+      a += y;
+    }
+    return a;
+  }
+  let Color1 = populate('#');
+  let Color2 = populate('#');
+  var angle = 'to right';
+
+  let gradient = 'linear-gradient(' + angle + ',' + Color1 + ', ' + Color2 + ")";
+  document.body.style.background = gradient;
+}
+function reset() {
+  curr_time.textContent = "";
+  total_duration.textContent = "";
+  seek_slider.value = 0;
+}
+function playpauseTrack() {
+  isPlaying ? pauseTrack() : playTrack();
+}
+
+function playTrack() {
+  curr_track.play();
+  isPlaying = true;
+  track_art.classList.add('rotate');
+  wave.classList.add('loader');
+  playpause_btn.innerHTML = '<i class="fa fa-pause-circle fa-4x"></i>';
+}
+
+function pauseTrack() {
+  curr_track.pause();
+  isPlaying = false;
+  track_art.classList.remove('rotate');
+  wave.classList.remove('loader');
+  playpause_btn.innerHTML = '<i class="fa fa-play-circle fa-4x"></i>';
+}
+
+function setUpdate() {
+  let seekPosition = 0;
+  if (!isNaN(curr_track.duration)) {
+    seekPosition = curr_track.currentTime * (100 / curr_track.duration);
+    seek_slider.value = seekPosition;
+
+    let currentMinutes = Math.floor(curr_track.currentTime / 60);
+    let currentSeconds = Math.floor(curr_track.currentTime - currentMinutes * 60);
+    let durationMinutes = Math.floor(curr_track.duration / 60);
+    let durationSeconds = Math.floor(curr_track.duration - durationMinutes * 60);
+
+    if (currentSeconds < 10) { currentSeconds = "0" + currentSeconds; }
+    if (durationSeconds < 10) { durationSeconds = "0" + durationSeconds; }
+    if (currentMinutes < 10) { currentMinutes = "0" + currentMinutes; }
+    if (durationMinutes < 10) { durationMinutes = "0" + durationMinutes; }
+
+    curr_time.textContent = currentMinutes + ":" + currentSeconds;
+    total_duration.textContent = durationMinutes + ":" + durationSeconds;
+  }
+}
